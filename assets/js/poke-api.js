41f93a1cf79f01cabdb07ee73814e5b8d2d16a30
Renato Teixeira <renato.gfteixeira@outlook.com>
2023-03-14 13:10:28 -0300

    Update poke-api.js
    Set the PokeAPI requests and its response treatment

---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+const POKE_API = {};
+
+function convertPokemonModel(pokeApiPokemon) {
+    const POKEMON = new Pokemon();
+
+    POKEMON.id = pokeApiPokemon.id;
+    POKEMON.name = pokeApiPokemon.name;
+
+    const TYPES = pokeApiPokemon.types.map((typeSlot) => typeSlot.type.name);
+    const [MAIN_TYPE] = TYPES;
+
+    POKEMON.types = TYPES;
+    POKEMON.mainType = MAIN_TYPE;
+    POKEMON.picture = pokeApiPokemon.sprites.other.dream_world.front_default;
+
+    return POKEMON;
+}
+
+POKE_API.getPokemonInfos = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokemonModel);
+}
+
+POKE_API.getPokemonsList = (offset = 0, limit = 0) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
+    return fetch(URL)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemonsList) => pokemonsList.map(PokeAPI.getPokemonInfosList))
+        .then((pokemonDetailsListRequests) => Promise.all(pokemonDetailsListRequests))
+        .then((pokemonDetailsList) => pokemonDetailsList)
+        .catch((error) => console.error(error));
+}

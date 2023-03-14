3e92e48870c4b2fadec297861f3c22d1cf13913d
Renato Teixeira <renato.gfteixeira@outlook.com>
2023-03-14 14:43:02 -0300

    Update pokemon-model.js
    Custom Pokemon model update to support the Pokemon detail information

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -4,4 +4,16 @@ class Pokemon {
     types = [];
     mainType;
     picture;
+    abilities = [];
+    height;
+    weight;
+    stats = {
+        health: null,
+        attack: null,
+        defense: null,
+        spAttack: null,
+        spDefense: null,
+        speed: null,
+        total: null
+    };
 }

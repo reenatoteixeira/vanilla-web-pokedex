5358f5df32926b1460bcd5089cb11842cb33734f
Renato Teixeira <renato.gfteixeira@outlook.com>
2023-03-14 14:46:10 -0300

    Update poke-api.js
    Update the Convert Pokemon Model function to convert the Pokemon detail information and create method to retrieve those detail information

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -12,6 +12,18 @@ function convertPokemonModel(pokeApiPokemon) {
     POKEMON.types = TYPES;
     POKEMON.mainType = MAIN_TYPE;
     POKEMON.picture = pokeApiPokemon.sprites.other.dream_world.front_default;
+    POKEMON.abilities = pokeApiPokemon.abilities.map((abilitieSlot) => abilitieSlot.ability.name);
+    POKEMON.height = (pokeApiPokemon.height * 10) / 100;
+    POKEMON.weight = (pokeApiPokemon.weight * 100) / 1000;
+    POKEMON.stats['health'] = pokeApiPokemon.stats[0].base_stat;
+    POKEMON.stats['attack'] = pokeApiPokemon.stats[1].base_stat;
+    POKEMON.stats['defense'] = pokeApiPokemon.stats[2].base_stat;
+    POKEMON.stats['spAttack'] = pokeApiPokemon.stats[3].base_stat;
+    POKEMON.stats['spDefense'] = pokeApiPokemon.stats[4].base_stat;
+    POKEMON.stats['speed'] = pokeApiPokemon.stats[5].base_stat;
+    POKEMON.stats['total'] = POKEMON.stats['health'] + POKEMON.stats['attack'] +
+        POKEMON.stats['defense'] + POKEMON.stats['spAttack'] +
+        POKEMON.stats['spDefense'] + POKEMON.stats['speed']
 
     return POKEMON;
 }
@@ -31,5 +43,15 @@ POKE_API.getPokemonsList = (offset = 0, limit = 0) => {
         .then((pokemonsList) => pokemonsList.map(POKE_API.getPokemonInfos))
         .then((pokemonDetailsListRequests) => Promise.all(pokemonDetailsListRequests))
         .then((pokemonDetailsList) => pokemonDetailsList)
+        .catch((error) => console.error(error));
+}
+
+POKE_API.getPokemonDetails = (pokemonId) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+    return fetch(URL)
+        .then((response) => response.json())
+        .then(convertPokemonModel)
+        .then((pokemon) => pokemon)
         .catch((error) => console.error(error));
 }

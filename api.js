10b55dd743ddfc4c880aa8299ae55bcee29a2903
Renato Teixeira <renato.gfteixeira@outlook.com>
2024-07-17 20:30:30 -0300

    updates

--- api.js
+++ api.js
@@ -2,8 +2,22 @@ const POKE_API = {};
 
 function convertPokemon(pokeApiPokemon) {
   const POKEMON = new Pokemon();
+  POKEMON.id = pokeApiPokemon.id;
   POKEMON.name = pokeApiPokemon.name;
-
+  POKEMON.types = pokeApiPokemon.types.map((type) => type.type.name);
+  POKEMON.mainType = POKEMON.types[0];
+  POKEMON.picture = pokeApiPokemon.sprites.other.dream_world.front_default;
+  POKEMON.abilities = pokeApiPokemon.abilities.map((ability) => ability.ability.name);
+  POKEMON.height = pokeApiPokemon.height;
+  POKEMON.weight = pokeApiPokemon.weight;
+  POKEMON.stats.health = pokeApiPokemon.stats[0].base_stat;
+  POKEMON.stats.attack = pokeApiPokemon.stats[1].base_stat;
+  POKEMON.stats.defense = pokeApiPokemon.stats[2].base_stat;
+  POKEMON.stats.specialAttack = pokeApiPokemon.stats[3].base_stat;
+  POKEMON.stats.specialDefense = pokeApiPokemon.stats[4].base_stat;
+  POKEMON.stats.speed = pokeApiPokemon.stats[5].base_stat;
+  POKEMON.stats.total = pokeApiPokemon.stats.reduce((total, stat) => total + stat.base_stat, 0);
+  
   return POKEMON;
 }
 
@@ -15,7 +29,7 @@ POKE_API.getPokemonInfo = (pokemon) => {
 
 POKE_API.getPokemonsList = (limit = 10, offset = 0) => {
   const URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-
+  
   return fetch(URL)
     .then((response) => response.json())
     .then((json) => json.results)
--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const POKEMON_LIST = document.getElementById("pokemon-list"),
   LOAD_MORE_BTN = document.getElementById("load-more-btn"),
-  LIMIT = 10;
+  MAX_POKEMONS_SHOWN = 10,
+  LIMIT = 4;
 
 let offset = 0;
 
-POKE_API.getPokemonsList(LIMIT, offset).then((pokemonsList = []) => {
-  POKEMON_LIST.innerHTML += pokemonsList
-    .map((pokemon) => `<li>${pokemon.name}</li>`)
-    .join("");
-});
+function loadPokemons(limit, offset) {
+  POKE_API.getPokemonsList(limit, offset).then((pokemonsList = []) => {
+    POKEMON_LIST.innerHTML += pokemonsList
+      .map((pokemon) => `
+      <li class="pokemon-card ${pokemon.mainType}">
+        <img class="pokemon-picture" src="${pokemon.picture}"
+          alt="Bulbasaur">
+        <h2>${pokemon.name} - ID ${pokemon.id}</h2>
+        <span>${pokemon.mainType}</span>
+      </li>
+      `)
+      .join("");
+  });
+}
+
+loadPokemons(LIMIT, offset)
+
+LOAD_MORE_BTN.addEventListener("click", () => {
+  offset += LIMIT;
+  const POKEMONS_SHOWN = offset + LIMIT;
+  
+  if (POKEMONS_SHOWN >= MAX_POKEMONS_SHOWN) {
+    const NEW_POKEMONS_SHOWN = MAX_POKEMONS_SHOWN - offset;
+    loadPokemons(NEW_POKEMONS_SHOWN, offset);
+    LOAD_MORE_BTN.style.display = "none";
+  } else {
+    loadPokemons(LIMIT, offset);
+  }
+})
